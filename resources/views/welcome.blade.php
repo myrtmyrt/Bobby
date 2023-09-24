6f204421e79649c3dda73dfff91d13dfebdec480
myrtilleknockaert <mmyrtille.knockaert@gmail.com>
2023-09-24 19:18:24 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased">
+        <div class="flex h-screen justify-center items-center gap-6">
+            Myrtille
+            <button class="bg-red-50 rounded-full">Connection</button>
+        </div>
+
+    </body>
+</html>
