ecec780997025724a33c6756d103689ea162847b
myrtilleknockaert <mmyrtille.knockaert@gmail.com>
2023-11-24 13:36:59 +0100

    Début de la page matériel, création du component objet

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,4 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'login'])->name("login");
 Route::get('/logout', [LoginController::class, 'logout'])->name("logout");
 Route::get('/', [HomeController::class, 'home'])->name("home");
-Route::get('/materiel',function (){return view('materiel');});
-//Route::get('/materiel', [\App\Http\Controllers\MaterielController::class, 'index'])->name("materiel");
 
