b10d7e6bd232c18be0aa7da4484d566f052ba8a1
pierre adorni <pierreadorni@free.fr>
2023-12-01 13:53:07 +0100

    /materiel with search and asso filter

--- app/Http/Controllers/MaterielController.php
+++ app/Http/Controllers/MaterielController.php
@@ -17,14 +17,38 @@ class MaterielController extends Controller
 
     public function getClassByName($name, Request $request){
         //$result = ItemClass->where('name', 'like', '%'.$name.'%');
-        $result = ItemClass::where('name', 'like', '%'.$name.'%')->get();
-        return view('materiel',  ['classes' => $result, 'request'=>$request]);
+        $result = ItemClass::where('asso_id', 'like', "%".$name."%")->get();
+        return view('materiel',  ['classes' => $result]);
+    }
+
+    public function getClasses(Request $request) {
+        // if ?query, search by itemclass name
+        // if ?asso_id, return only classes from this asso
+
+        $query = $request->input('query');
+        $asso_id = $request->input('asso_id');
+
+        $classes = ItemClass::query();
+        if ($query) {
+            $classes = $classes->where('name', 'like', '%'.$query.'%');
+        }
+        if ($asso_id) {
+            $classes = $classes->where('asso_id', $asso_id);
+        }
+
+        return view('materiel', ['classes' => $classes->get()]);
+    }
+
+    public function searchClassesByAsso(Request $request) {
+        $query = $request->input('query');
+        $result = ItemClass::where('asso_id', 'like', "%".$query."%")->get();
+        return view('materiel', ['classes' => $result]);
     }
 
     public function getAssoItems(Request $request){
         $asso = $request->input('asso_id');
         $class_id = ItemClass::where('asso_id',$asso)->with('items')->get();
-        dd($class_id);
+//        dd($class_id);
         $item = new Item();
         $result = $item->itemClass($class_id);
         return view('materiel', ['items' => $result]);
