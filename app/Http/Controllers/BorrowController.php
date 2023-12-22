fd6ae466661a348428f8e1296c7fb6c99aea6081
Oceane <oceane.poussin@etu.utc.fr>
2023-12-22 11:41:07 +0100

    creation borrowRequests view

--- app/Http/Controllers/BorrowController.php
+++ app/Http/Controllers/BorrowController.php
@@ -9,18 +9,23 @@ use Carbon\Carbon;
 
 class BorrowController extends Controller
 {
-    public function requests(){
+   /* public function requests(){
         $result = BorrowRequest::all();
         return view('borrowRequests', ['requests' => $result]);
-    }
+    }*/
+    // changer de vue --> vue pour voir ses borrow requests
 
+    public function getForm(/*$item_id*/){
+        return view('borrowRequests'/*, ['item_id'=>$item_id]*/);
+    }
     public function addRequest(Request $request)
     {
         $session = session('user');
-        $asso_id = $session['id'];
-        $debut_date = $request->query('debut_date');
-        $end_date = $request->query('end_date');
-        $comment = $request->query('comment');
+/*        $asso_id = $session['id'];*/
+        $asso_id = $request->input('asso_id');
+        $debut_date = $request->input('debut_date');
+        $end_date = $request->input('end_date');
+        $comment = $request->input('comment');
         $borrowRequest = new BorrowRequest([
             'asso_id' => $asso_id,
             'debut_date' => $debut_date,
