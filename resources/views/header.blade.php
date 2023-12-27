747f6b75ec1de84ae43ef585c832f10d486cf1c5
myrtilleknockaert <mmyrtille.knockaert@gmail.com>
2023-12-27 15:25:46 +0100

    màj header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -44,6 +44,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <header class="bg-#FFFFFF text-#73747A p-3 flex items-center content-center justify-between">
@@ -51,30 +52,61 @@
         <img src="{{ asset('assets/logoBobby.png') }}" alt="Logo" class="h-12">
     </a>
     <nav class="flex items-center">
-        <a href="/materiel" class="mr-4 hover:text-[#D90368]">Materiel</a>
-        <a href="/inventaire" class="mr-4 hover:text-[#D90368]">Inventaire</a>
+        <div class="relative group">
+            <a href="/materiel" class="mr-4 hover:text-[#D90368]">Matériel</a>
+            <div class="absolute hidden mt-2 space-y-2 bg-white rounded-md shadow-md border border-gray-300 w-40">
+                <a href="/materiel/evenementiel" class="block px-4 py-2 hover:text-[#D90368]">Evènementiel</a>
+                <a href="/materiel/decoration" class="block px-4 py-2 hover:text-[#D90368]">Décoration</a>
+                <a href="/materiel/meubles" class="block px-4 py-2 hover:text-[#D90368]">Meubles</a>
+                <a href="/materiel/deguisements" class="block px-4 py-2 hover:text-[#D90368]">Déguisements</a>
+            </div>
+        </div>
         <a href="/emprunts" class="mr-4 hover:text-[#D90368]">Mes emprunts</a>
         <a href="/myAsso" class="mr-4 hover:text-[#D90368]">Mon Association</a>
-        <a href="/login">
-            <button class="mt-4 ml-4 bg-[#D90368] text-white px-4 py-2 rounded hover:bg-sky-700">Connexion</button>
-        </a>
-        <button class="ml-4 w-8 h-8 bg-[#D90368] rounded-full flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg>
-
-        </button>
-        <a href="/logout">
-            <button class="ml-4 w-8 h-8 bg-[#D90368] rounded-full flex items-center justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                </svg>
-
-            </button>
-        </a>
-
+        @if (session("user"))
+            <!-- User is logged in, show logout button -->
+            <a href="/logout">
+                <button class="ml-4 w-8 h-8 bg-[#D90368] rounded-full flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                    </svg>
+                </button>
+            </a>
+        @else
+            <!-- User is not logged in, show login button -->
+            <a href="/login">
+                <button class="mt-4 ml-4 bg-[#D90368] text-white px-4 py-2 rounded hover:bg-sky-700">Connexion</button>
+            </a>
+        @endif
     </nav>
 
-
 </header>
+
+<script>
+    // JavaScript pour afficher/cacher le sous-menu au survol
+    const group = document.querySelector('.group');
+    const subMenu = document.querySelector('.absolute');
+
+    let mouseTimer;
+
+    group.addEventListener('mouseover', function() {
+        clearTimeout(mouseTimer);
+        subMenu.classList.remove('hidden');
+    });
+
+    group.addEventListener('mouseleave', function() {
+        mouseTimer = setTimeout(() => {
+            subMenu.classList.add('hidden');
+        }, 200); // Délai en millisecondes avant de masquer le sous-menu
+    });
+
+    // Annuler le délai si la souris revient sur le sous-menu
+    subMenu.addEventListener('mouseover', function() {
+        clearTimeout(mouseTimer);
+    });
+
+    // Cacher le sous-menu s'il n'est pas survolé
+    subMenu.addEventListener('mouseleave', function() {
+        subMenu.classList.add('hidden');
+    });
+</script>
