f243b63b8aced320be796974d47f441a6ba5c19c
Oceane <oceane.poussin@etu.utc.fr>
2024-01-11 05:14:26 +0100

    update

--- resources/views/borrowRequests.blade.php
+++ resources/views/borrowRequests.blade.php
@@ -21,7 +21,6 @@
 
     <div class="pt-20">
         <div class="container p-4">
-            @dump($class->id)
             <form method="post" action="/demandeEmprunt/{{$class->id}}" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 @csrf
                 <div>
